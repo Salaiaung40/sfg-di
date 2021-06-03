899d3f8a5d97e06b85021096ccaf3b5f3570dd2c
Salaiaung40 <salaihtunlin@hotnail.com>
2021-06-03 12:43:15 +0200

    I18n with Default Profile

--- src/main/java/salai/springframework/sfg/di/services/I18nSpanishGreetingService.java
+++ src/main/java/salai/springframework/sfg/di/services/I18nSpanishGreetingService.java
@@ -4,7 +4,7 @@ package salai.springframework.sfg.di.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("ES")
+@Profile({"ES", "default"})
 @Service("i18nService") // it is combine writing of Service + Qualifier
 public class I18nSpanishGreetingService implements GreetingService{
     @Override
