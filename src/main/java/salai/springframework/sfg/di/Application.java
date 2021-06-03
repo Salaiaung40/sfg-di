9caae229c3e3dbccb65c84f792710816054460be
Salaiaung40 <salaihtunlin@hotnail.com>
2021-06-03 11:23:31 +0200

    with the Primary annotation

--- src/main/java/salai/springframework/sfg/di/Application.java
+++ src/main/java/salai/springframework/sfg/di/Application.java
@@ -11,36 +11,40 @@ import salai.springframework.sfg.di.controllers.SetterInjectedController;
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		ApplicationContext ctx= SpringApplication.run(Application.class, args);
-		MyController myController = (MyController) ctx.getBean("myController");
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+        MyController myController = (MyController) ctx.getBean("myController");
 
+		/*
 		String greeting = myController.sayHello();
 
 		System.out.println(greeting);
+        */
+        System.out.println("------------- Primary bean");
+        System.out.println(myController.sayHello());
 
-		// working with springframework DI
+        // working with springframework DI
 
-		System.out.println(" ....... Property" );
+        System.out.println(" ....... Property");
 
-		PropertyInjectedController propertyInjectedController = (PropertyInjectedController)  ctx.getBean("propertyInjectedController");
+        PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
 
-		System.out.println(propertyInjectedController.getGreeting());
+        System.out.println(propertyInjectedController.getGreeting());
 		/* This will get error coz spring does not know that the manage component.
 		Thus you need to assign the annotated "@Controller" at PropertyInjectedController Class.
 		And that add "@Autowired" to initialisation (public GreetingService greetingService).
 		 */
 
-		System.out.println(" ....... Setter" );
-		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
+        System.out.println(" ....... Setter");
+        SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
 
-		System.out.println(setterInjectedController.getGreeting());
+        System.out.println(setterInjectedController.getGreeting());
 
-		System.out.println(" ....... Constructor" );
-		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController)  ctx.getBean("constructorInjectedController");
+        System.out.println(" ....... Constructor");
+        ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
 
-		System.out.println(constructorInjectedController.getGreeting());
-	}
+        System.out.println(constructorInjectedController.getGreeting());
+    }
 
 }
--- src/main/java/salai/springframework/sfg/di/controllers/MyController.java
+++ src/main/java/salai/springframework/sfg/di/controllers/MyController.java
@@ -2,12 +2,21 @@ package salai.springframework.sfg.di.controllers;
 //Very basic and direct controller but not recommend to use it
 
 import org.springframework.stereotype.Controller;
+import salai.springframework.sfg.di.services.GreetingService;
 
 @Controller
 public class MyController {
+
+    private final GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
     public String sayHello(){
-        System.out.println("Hello World from Salai!!");
-        return "Hi Folks!!";
+//        System.out.println("Hello World from Salai!!");
+//        return "Hi Folks!!";
+        return greetingService.sayGreeting();
     }
 
 }
